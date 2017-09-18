2dd0b3fc05639a5aee752a15ed906f4ffed67999
Cytoscript <cytoscript@gmail.com>
2017-09-18 13:17:07 +0530

    =initial commit

---
+++ server.js
@@ -0,0 +1,64 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname +'/views/partials');
+
+app.set('view engine', 'hbs');
+
+
+app.use(function(req,res,next){
+    res.render('maintenance.hbs');
+});
+
+app.use((req,res,next)=>{
+    var now = new Date().toString();
+    var log = now+req.method+req.url;
+    fs.appendFile('server.log',log+'/n',function(err){
+        if(err){
+            console.log('Error occured');
+        }
+    });
+    console.log(log);
+    next();
+});
+
+app.use(express.static(__dirname+'/public'));
+
+
+
+
+hbs.registerHelper('getCurrentYear',function(){
+    return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', function(text){
+    return text.toUpperCase();
+});
+
+
+
+app.get('/',function(req,res){
+    res.render('home.hbs',{
+        pageTitle:'Home'
+    })
+});
+
+app.get('/about',function(req,res){
+    res.render('about.hbs',{
+        pageTitle:'About Page',
+    });
+
+});
+
+app.get('/bad', function(req,res){
+    res.send({
+        error:'Error occured'
+    });
+});
+
+app.listen(3000,function(){
+    console.log('Server is up on port 3000');
+});
